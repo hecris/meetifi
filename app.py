e7dc244f8503dc76ebd037114c374bc31155c17b
hecris <christopher.he33@myhunter.cuny.edu>
2018-09-30 08:49:33 -0400

    mvc

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from queries import *
 import datetime
 import requests
 from urllib.parse import quote
+import re
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
 
 app = Flask(__name__)
 
@@ -11,6 +17,12 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+    user = request.form['username']
+    return redirect(url_for('user_home', user=user))
+    
 
 @app.route('/home/<user>')
 def user_home(user):
@@ -41,7 +53,7 @@ def create():
     address = request.form['address']
     meetup_date = request.form['meetup_date']
     meetup_time = request.form['meetup_time']
-    invites = request.form['invites'].replace('\r\n', '*')
+    invites = request.form['invites'].replace('\r\n', ', ')
     with sqlite3.connect('database.db') as con:
         cur = con.cursor()
         cur.execute(add_meetup_query,
@@ -50,8 +62,8 @@ def create():
         
     return user_home(user)
 
-@app.route('/meetup/<name>')
-def existing_meetup(name):
+@app.route('/meetup/<user>/<name>')
+def existing_meetup(user, name):
     con = sqlite3.connect('database.db')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
@@ -59,30 +71,57 @@ def existing_meetup(name):
     row = cur.fetchall()[0]
     address = row['address']
     date = row['meetup_date']
+    dest_geo = get_geo(address)
+    weathers = get_hourly_weather(dest_geo.split(',')[0],dest_geo.split(',')[1], date)
+    position = get_position()
+    
+    try:
+        route = get_route(position, dest_geo)
+        start = route['response']['route'][0]['leg'][0]['maneuver']
+    except:
+        return 'could not find route, please check address'
+    directions = []
+
+    
+    for s in start:
+        directions.append(remove_tags(s['instruction']))
+
+    json_restaurants = get_places(dest_geo, 'restaurant')
+    restaurants = json_restaurants['results']['items']
+    
+    return render_template('meetup.html', info = row, weathers = weathers, user = user, directions = directions, restaurants = restaurants)
+
+def get_geo(address):
     r = requests.get(geocoder_url + quote(address)).json()
     lat = str(r['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Latitude'])
     long = str(r['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Longitude'])
-    weathers = get_hourly_weather(lat,long, date)
-    return render_template('meetup.html', info = row, weathers = weathers)
+    return lat + "," + long
 
 def get_hourly_weather(lat, long, date):
     r = requests.get(weather_url + "latitude=" + lat + "&longitude=" + long).json()
     #return weather_url + "latitude=" + lat + "&longitude=" + long
     #return str(r)
     days = r['hourlyForecasts']['forecastLocation']['forecast']
+
     weathers = []
     for d in days:
         if date in d['utcTime']:
             weathers.append(d)
     return weathers
 
-def get_positioning():
-    r = requests.post(position_url, json=position_data).json()
-    location = r['location']
-    lat = location['lat']
-    long = location['lng']
-    return (lat, long)
-    
+def get_position():
+    ip = requests.get('http://bot.whatismyipaddress.com/').text
+    r = requests.get('http://ip-api.com/json/' + ip).json()
+    return str(r['lat']) + ',' + str(r['lon'])
+
+def get_route(cur, des):
+
+    r = requests.get(routing_url % (cur, des))
+    return r.json()
+
+def get_places(at, query):
+    r = requests.get(places_url % (at, query))
+    return r.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- queries.py
+++ queries.py
@@ -13,10 +13,8 @@ WHERE name = ?"""
 
 
 geocoder_url = "https://geocoder.api.here.com/6.2/geocode.json?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA&searchtext="
+
 weather_url = "https://weather.api.here.com/weather/1.0/report.json?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA&product=forecast_hourly&"
-position_url = "https://pos.api.here.com/positioning/v1/locate?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA"
-position_data = {
-  "wlan": [
-  {"mac": "F4-55-95-11-2C-C1"}
-  ]
-}
+routing_url = """https://route.api.here.com/routing/7.2/calculateroute.json?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA&waypoint0=geo!%s&waypoint1=geo!%s&departure=now&mode=fastest;publicTransport&combineChange=true"""
+places_url = """
+https://places.cit.api.here.com/places/v1/discover/search?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA&at=%s&q=%s"""
