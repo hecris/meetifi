446843fed6fdcd5a9d71c8d46acf5e88943f08d2
hecris <christopher.he33@myhunter.cuny.edu>
2018-09-29 23:19:36 -0400

    add everything

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 import sqlite3
 from queries import *
+import datetime
+from urllib.parse import quote
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def root():
-    
     return render_template('index.html')
 
 @app.route('/home/<user>')
@@ -15,30 +17,51 @@ def user_home(user):
     con = sqlite3.connect('database.db')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute(get_meetups_query, [user])
+    cur.execute(get_all_meetups_query, (user, '%'+user+'%'))
     rows = cur.fetchall()
-    return render_template('home.html', meetups = rows)
-
-@app.route('/newmeetup')
-def new_meetup():
+    upcoming = []
+    previous = []
+    for r in rows:
+        if datetime.datetime.strptime(r['meetup_date'], '%Y-%m-%d') >= datetime.datetime.today():
+            upcoming.append(r)
+        else:
+            previous.append(r)
     
-    return render_template('newmeetup.html')
+    return render_template('home.html', upcoming = upcoming, previous = previous, user = user)
+
+@app.route('/newmeetup/<user>')
+def new_meetup(user):
+    
+    return render_template('newmeetup.html', user = user)
 
 @app.route('/create', methods=['POST'])
 def create():
-    usr = 'chris'
+    user = request.form['user']
     name = request.form['name']
     address = request.form['address']
     meetup_date = request.form['meetup_date']
     meetup_time = request.form['meetup_time']
-    invites = request.form['invites']
+    invites = request.form['invites'].replace('\r\n', '*')
     with sqlite3.connect('database.db') as con:
         cur = con.cursor()
         cur.execute(add_meetup_query,
-                    [usr, name, address, meetup_date, meetup_time, invites])
+                    [user, name, address, meetup_date, meetup_time, invites])
         con.commit()
         
-    return str(request.form)
+    return 'done'
+
+@app.route('/meetup/<name>')
+def existing_meetup(name):
+    con = sqlite3.connect('database.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute(get_meetup_query, (name,))
+    row = cur.fetchall()[0]
+    address = row['address']
+    r = requests.get(geocoder_url + quote(address))
+    print(r.text)
+    return render_template('meetup.html', info = row)
+
 
 
 if __name__ == '__main__':
--- queries.py
+++ queries.py
@@ -3,6 +3,13 @@ add_meetup_query = """
 INSERT INTO meetups(user, name, address, meetup_date, meetup_time, invites)
 VALUES (?, ?, ?, ?, ?, ?)"""
 
-get_meetups_query = """
+get_all_meetups_query = """
 SELECT * FROM meetups
-WHERE user = ?"""
+WHERE user = ? OR invites LIKE ?"""
+
+get_meetup_query = """
+SELECT * FROM meetups
+WHERE name = ?"""
+
+
+geocoder_url = "https://geocoder.api.here.com/6.2/geocode.json?app_id=uhIYrWPnuNlLk1tUWsfd&app_code=4DxLDAVJdKZy6uLvWof3jA&searchtext="
