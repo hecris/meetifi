69e9a79c001596fb60ed4c80989621a3f3121a84
hecris <christopher.he33@myhunter.cuny.edu>
2018-10-01 02:44:05 -0400

    pls

--- app.py
+++ app.py
@@ -134,4 +134,5 @@ def get_places(at, query):
     return r.json()
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
